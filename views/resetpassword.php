dcd6b0d85978e2705383bb6a4f2e6c2be3cad9c0
rumu5550 <rumaiyaislamsadia@gmail.com>
2025-12-24 13:36:50 +0600

    Collaboration done

---
+++ views/resetpassword.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Reset Password</title>
+    <link rel="stylesheet" href="../assets/css/style.css" />
+  </head>
+  <body>
+    <div class="flex">
+       <div class="container">
+          <h4>Reset Password</h4>
+    <h3>Create a new password</h3>
+    <div>
+      <label for="new password">New Password</label>
+      <br />
+      <input type="password" name="password" />
+      <br />
+      <br />
+      <label for="confirm password">Confirm Password</label>
+      <br />
+      <input type="password" name="conform password" />
+    </div>
+    <br />
+    <br />
+    <div>
+      <a href="signin.php"><button>Reset Password</button></a>
+    </div>
+       </div>
+    </div>
+  </body>
+</html>
---
+++ views/signup.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="../assets/css/style.css" />
+  </head>
+  <body>
+    <div class="flex">
+      <div class="container">
+        <div class="heading">
+          <h3>Creat Account</h3>
+          <h4>Join the Lifestyle Achivement Passport</h4>
+        </div>
+        <div class="namefield">
+          <div>
+            <label for="fname">First Name</label>
+            <br />
+            <input
+              id="fname"
+              type="text"
+              name="First Name"
+              onblur="testname()"
+            />
+          </div>
+          <div>
+            <label for="lname">Last Name</label>
+            <br />
+            <input
+              id="lname"
+              type="text"
+              name="Last Name"
+              onblur="testname()"
+            />
+          </div>
+        </div>
+        <div id="nameError"></div>
+        <br />
+        <div>
+          <label for="email">Email</label>
+          <br />
+          <input id="email" type="email" name="email" 
+          onblur="validateEmail()"/>
+          <p id="emailerror"></p>
+          <label for="password">Password</label>
+          <br />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onblur="testPassword()"
+          />
+          <p id="pError"></p>
+
+          <label for="confirmPassword">Confirm Password</label>
+          <br />
+          <input id="cpassword" type="password" name="confirmPassword" onblur="confirmPassword()" />
+          <p id="cperror"></p>
+        </div>
+        <br />
+        <div>
+          <input type="submit" value="Creat Account" />
+        </div>
+        <div>
+          <h4>Already have an account? <a href="signin.php">Sign In</a></h4>
+        </div>
+      </div>
+    </div>
+    <script src="../javascript/script.js"></script>
+  </body>
+</html>
