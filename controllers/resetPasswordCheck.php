c0928c2bc18d4150953f1187795dcb8cc312a3d7
rumu5550 <rumaiyaislamsadia@gmail.com>
2026-01-07 12:01:27 +0600

    reset password

---
+++ controllers/resetPasswordCheck.php
@@ -0,0 +1,27 @@
+<?php
+    require_once('../models/userModel.php');
+    session_start();
+
+    if(isset($_POST['submit'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm-password'];
+
+        if($email == "" || $password == "" || $confirm_password == ""){
+            echo "Null value... please type again!";
+        } else if($password != $confirm_password){
+            echo "Passwords do not match!";
+        } else {
+            
+            
+            $status = resetPassword($email, $password);
+            if($status){
+                header('location: ../views/signin.php');
+            } else {
+                echo "Failed to reset password. Please try again.";
+            }
+        }
+    } else {
+        header('location: ../views/resetpassword.php');
+    }
+?>
